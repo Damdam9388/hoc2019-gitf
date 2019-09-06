1ea3ab6a3a285be62a3418aa868a0e7b6d44a978
Damien <damien.terro@gmail.com>
2019-09-06 17:04:03 +0200

    last version

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,14 +3,32 @@
 namespace App\DataFixtures;
 
 use App\Entity\Gift;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
+    private $encoder;
+
+    public function __construct(UserPassxordEncoderInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
     public function load(ObjectManager $manager)
     {
+        $user = new User();
+
+        $user->setEmail('bob@bob.com')
+            ->setRoles(['ROLE_ADMIN'])
+            ->setPassword($this->encoder->encodePassword(
+                $user,
+                "blablabla"
+            ));
+            $manager->persist($user);
+            $manager->flush();
+
         $faker = Factory::create('fr_FR');
 
         for ($i = 0; $i < 20; $i++) {
